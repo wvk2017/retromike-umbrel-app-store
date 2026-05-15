b8311f7100a14b649bd980e31fae1e3100f89550
wvk2017 <58895020+wvk2017@users.noreply.github.com>
2026-05-15 19:49:05 +0200

    Delete retro-mike-miningcore-webui-legacy directory

--- retro-mike-miningcore-webui-legacy/exports.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p /home/umbrel/.miningcore_legacy
-mkdir -p /home/umbrel/umbrel/app-data/retro-mike-vtc-node/.vertcoin
-chown -R 1000:1000 /home/umbrel/.miningcore_legacy
-chown -R 1000:1000 /home/umbrel/umbrel/app-data/retro-mike-vtc-node/.vertcoin
